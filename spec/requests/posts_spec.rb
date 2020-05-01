4fe99483cba096223b053072e721fc07005d3102
Julia de L <jldelorimier@gmail.com>
2020-04-30 21:58:59 -0400

    added create action in rspec with fake user jwt and fake post

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,12 +5,36 @@ RSpec.describe "Posts", type: :request do
     it "returns all the tests in the test database" do
       user = User.create!(name: "Kerry", email: "kerry@gmail.com", password: "password")
       
-      Post.create!(title: "first test post", body: "this is the body of the first test post", image: "this is the image URL for the first test post", user_id: user.id)
+      Post.create!(title: "first test post index", body: "this is the body of the first test post in my index action", image: "this is the image URL for the first test post in my index action", user_id: user.id)
+      
       get "/api/posts"
 
       p JSON.parse(response.body)
 
       expect(response).to have_http_status(200)
+    end
+  end
+  describe "POST /api/posts" do
+    it "should create a new post in the db" do
+      user = User.create!(name: "Kerry", email: "kerry@gmail.com", password: "password")
+
+      jwt = JWT.encode(
+        {
+          user: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        "random", # the secret key
+        'HS256' # the encryption algorithm
+      )
+
+      post '/api/posts', params: {
+        title: "title of first post in create action",
+        body: "body of first post in create action",
+        image: "image of first post in create action",
+      }, headers: {
+        Authorization: "Bearer #{jwt}"
+      }
+      
     end
   end
 end
